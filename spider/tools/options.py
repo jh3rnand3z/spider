ff057b6154e11fe1ea139a7b91211601518c88ee
Jean Chassoul <chassoul@gmail.com>
2014-10-14 15:31:05 -0600

    data options

--- spider/tools/options.py
+++ spider/tools/options.py
@@ -2,17 +2,24 @@ import os
 
 import tornado.options
 
+from tornado.options import parse_config_file
+
 # ownership missing
 
 config_path = 'spider.conf'
 
 def options():
     '''
-        Outbound campaigns configuration options
+        Inbound campaigns configuration options
     '''
     tornado.options.define('ensure_indexes', 
         default=True, type=bool,
         help=('Ensure collection indexes'))
+
+    # Set config and stuff
+    tornado.options.define('config',
+        type=str, help='path to config file',
+        callback=lambda path: parse_config_file(path, final=False))
 
     tornado.options.define('domain',
         default='iofun.io', type=str,
@@ -26,6 +33,31 @@ def options():
     tornado.options.define('port',
         default=8888, type=int,
         help=('Server port'))
+
+    # MongoDB database settings
+    tornado.options.define('mongo_host',
+        type=str, help=('MongoDB hostname or ip address'))
+
+    tornado.options.define('mongo_port',
+        default=27017, type=int,
+        help=('MongoDB port'))
+
+    # PostgreSQL database settings
+    tornado.options.define('sql_host',
+        type=str, help=('PostgreSQL hostname or ip address'))
+
+    tornado.options.define('sql_port',
+        default=5432, type=int,
+        help=('PostgreSQL port'))
+
+    tornado.options.define('sql_database',
+        type=str, help=('PostgreSQL database'))
+
+    tornado.options.define('sql_user',
+        type=str, help=('PostgreSQL username'))
+
+    tornado.options.define('sql_password',
+        type=str, help=('PostgreSQL username password'))
 
     tornado.options.define('base_url',
         default='api', type=str,
@@ -41,7 +73,7 @@ def options():
     
     tornado.options.define('retry_time',
         default=300, type=int,
-        help=('Outbound calling retry time'))
+        help=('Inbound calling retry time'))
 
     tornado.options.define('wait_time',
         default=45, type=int,
@@ -52,12 +84,12 @@ def options():
         help=('Maximum number of concurrent calls'))
 
     tornado.options.define('spool_dir',
-        default='/var/spool/asterisk/outgoing/', type=str,
-        help=('Asterisk spool dir'))
+        default='/var/spool/asterisk/inbound/', type=str,
+        help=('Asterisk inbound spool dir'))
 
     tornado.options.define('tmp_dir',
         default='/tmp/', type=str,
-        help=('tmp outbound call files'))
+        help=('tmp inbound stuff'))
 
     # Parse config file, then command line...
     # so command line switches take precedence
